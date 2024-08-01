ad8148c0e36fee410827008736fc36c82787bca3
MEMozki <memozki@bk.ru>
2024-08-01 14:10:49 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let pingInterval;
 document.getElementById('startPing').addEventListener('click', () => {
     const url = document.getElementById('urlInput').value;
     if (!url) {
-        alert('Please enter a DOMEN');
+        alert('Please enter a URL');
         return;
     }
     startPing(url);
@@ -19,28 +19,23 @@ document.getElementById('stopPing').addEventListener('click', () => {
 function startPing(url) {
     requestCount = 0;
     document.getElementById('requestCount').innerText = '0';
-    document.getElementById('pingTime').innerText = '0 ms';
     document.getElementById('startPing').disabled = true;
     document.getElementById('stopPing').disabled = false;
     updateProgress(0);
 
     pingInterval = setInterval(async () => {
-        const start = Date.now();
         try {
             await fetch(url, { mode: 'no-cors' });
-            const end = Date.now();
-            updatePing(end - start);
         } catch (error) {
-            const end = Date.now();
-            updatePing(end - start, false);
+            // Ignore errors
         }
+        updateRequestCount();
     }, 25); // Интервал в 1 миллисекунду
 }
 
-function updatePing(pingTime, success = true) {
+function updateRequestCount() {
     requestCount++;
     document.getElementById('requestCount').innerText = requestCount.toString();
-    document.getElementById('pingTime').innerText = success ? `${pingTime} ms` : 'Request failed';
     updateProgress((requestCount % 100) / 100 * 100);
 }
 
