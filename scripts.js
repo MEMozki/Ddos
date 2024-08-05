6891ae1ef651c08aaa33b7e2d1480eeb02043663
MEMozki <memozki@bk.ru>
2024-08-05 20:55:31 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const whiteList = ['pinterest.com', 'example.com']; // Список запрещенных сайтов
+const whiteList = ['65.108.3.108']; // Список запрещенных сайтов
 
 let requestCount = 0;
 let pingInterval;
@@ -26,28 +26,23 @@ document.getElementById('stopPing').addEventListener('click', () => {
 function startPing(url) {
     requestCount = 0;
     document.getElementById('requestCount').innerText = '0';
-    document.getElementById('pingTime').innerText = '0 ms';
     document.getElementById('startPing').disabled = true;
     document.getElementById('stopPing').disabled = false;
     updateProgress(0);
 
     pingInterval = setInterval(async () => {
-        const start = Date.now();
         try {
             await fetch(url, { mode: 'no-cors' });
-            const end = Date.now();
-            updatePing(end - start);
+            updateRequestCount();
         } catch (error) {
-            const end = Date.now();
-            updatePing(end - start, false);
+            updateRequestCount();
         }
-    }, 1); // Интервал в 1 миллисекунду
+    }, 25); // Интервал в 25 миллисекунду
 }
 
-function updatePing(pingTime, success = true) {
+function updateRequestCount() {
     requestCount++;
     document.getElementById('requestCount').innerText = requestCount.toString();
-    document.getElementById('pingTime').innerText = success ? `${pingTime} ms` : 'Request failed';
     updateProgress((requestCount % 100) / 100 * 100);
 }
 
